c17032c8d972e4c212d909ce724737d418261fac
Mateus Vidal <devmateusvidal@gmail.com>
2025-02-17 21:31:26 -0300

    February 17

---
+++ 02-variables, data types & more/04-data-types/script.js
@@ -0,0 +1,37 @@
+//String
+const firstName = 'Sara';
+
+//Number
+const age = 30;
+const temp = 98.9;
+
+//Boolen
+const hasKids = true;
+
+//Null
+const aptNumber = null;
+
+//Undefined
+// let score;
+const score = undefined;
+
+//Symbol
+const id = Symbol
+
+//BigInt
+const n = 54156146546546546545n;
+
+// Reference types
+const numbers = [1,2,3,4];
+
+const person = {
+  name: 'Brad'
+};
+
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
