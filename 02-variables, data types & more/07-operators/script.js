7d31dad59bf3733efff27dcc80051dbf7ce45197
Mateus Vidal <devmateusvidal@gmail.com>
2025-02-21 22:24:46 -0300

    February 21

---
+++ 02-variables, data types & more/07-operators/script.js
@@ -0,0 +1,53 @@
+//1. Arithmetic Operators
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// 2. Concatenation
+x = 'Hello' + ' ' + 'World';
+
+// 3. Exponent
+x = 2 ** 3;
+
+// 4. Increment
+x = 1;
+// x = x + 1;
+x++;
+
+// 5. Decrement 
+// x = x - 1;
+x--;
+
+// 6. Assigment Operators 
+
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+// 7. Comparison Operators
+
+x = 2 == 2;
+
+x = 2 === 2;
+
+x = 2 != '2';
+
+x = 2 !== 2;
+
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
