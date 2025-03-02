0ddf24c288f3fbe0314adda1d72fcd3611f3e3de
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-02 20:34:35 -0300

    March 2

--- 02-variables, data types & more/10-capitalize-challenge/script.js
+++ 02-variables, data types & more/10-capitalize-challenge/script.js
@@ -14,5 +14,7 @@ let myNewString;
 myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
 //Solution 2:
 myNewString = myString[0].toUpperCase() + myString.substring(1);
+//Solution 3:
+myNewString = myString[0].toUpperCase() + myString.substring(1);
 
 console.log(myNewString);
