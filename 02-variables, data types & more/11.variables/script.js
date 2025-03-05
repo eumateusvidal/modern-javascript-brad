2e6495c137805da3717e4993f8ac629f06fda0d6
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-05 17:08:14 -0300

    March 5

---
+++ 02-variables, data types & more/11.variables/script.js
@@ -0,0 +1,17 @@
+let x;
+
+const num = new Number(5.4567);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(2);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-US')
+
+console.log(x);
