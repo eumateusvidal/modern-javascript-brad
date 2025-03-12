833e3508133442aae2c93ffda48d65637f1027f6
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-11 22:51:18 -0300

    March 11

---
+++ 02-variables, data types & more/14-dates-and-times/script.js
@@ -0,0 +1,11 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2021, 6, 1995)
+
+d = new Date('2021-07-10')
+
+console.log(d);
---
+++ 02-variables, data types & more/15-date-objects/script.js
@@ -0,0 +1,33 @@
+let x;
+
+let d = new Date();
+
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+
+x = d.getMonth();
+x = d.getMonth() + 1;
+
+//It will get the day of the month
+x = d.getDate();
+
+//It will get the day of the week
+x = d.getDay();
+
+x = d.getHours();
+
+x = d.getMinutes();
+
+x = d.getSeconds();
+
+x = d.getMilliseconds();
+
+x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+
+
+
+console.log(x);
