01fbf2de40ec5eb88909468469fd196467f8aeaa
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-13 21:34:52 -0300

    March 13

--- 03-arrays-&-objects/04-array-challenge/script.js
+++ 03-arrays-&-objects/04-array-challenge/script.js
@@ -1,14 +1,17 @@
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-arr.reverse();
-arr.push(0);
-arr.unshift(6);
+// arr.reverse();
+// arr.push(0);
+// arr.unshift(6);
 
-console.log(arr)
+// console.log(arr)
 
-// const arr1 = [1, 2, 3, 4, 5];
-// const arr2 = [5, 6, 7, 8, 9, 10];
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
 
-// arr3 = [...arr1, ...arr2];
+const arr3 = arr1.slice (0, 4).concat(arr2);
 
-// console.log(arr3);
+const arr4 = [...arr1, ...arr2];
+arr4.splice(4, 1);
+
+console.log(arr4);
