95299f24157c47237a3c667dee9ba62afcf92ff6
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-17 23:45:08 -0300

    March 17

---
+++ 03-arrays-&-objects/05-object-literals/script.js
@@ -0,0 +1,34 @@
+const person = {
+  name: 'Jone Doe',
+  age:30,
+  isAdmin: true,
+  address: {
+   street: '123 Main st',
+   city: 'Boston',
+   state: 'MA',
+  },
+  hobbies: ['music', 'sports']
+
+};
+
+x = person.name;
+x = person ['age']
+x = person.address.state;
+x = person.hobbies[0];
+
+person.name = 'Jane Doe';
+person['isAdmin'] = false;
+
+delete person.age
+
+person.hasChildren = true
+
+person.greet = function () {
+  console.log(`Hello, my name is ${this.name}`)
+}
+
+person.greet();
+
+x = person;
+
+console.log(x);
