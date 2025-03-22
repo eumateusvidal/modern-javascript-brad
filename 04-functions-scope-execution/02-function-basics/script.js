de6e6f09acaee87a0af695065eeb273f5a7cd955
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-21 21:58:14 -0300

    March 21

---
+++ 04-functions-scope-execution/02-function-basics/script.js
@@ -0,0 +1,23 @@
+function sayHello () {
+  console.log('Hello World');
+}
+
+sayHello(); //Calling a function
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+} //Declaring a function
+
+add(5, 10)
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+subtract(10, 2)
+
+const result = subtract(10,2)
+
+console.log(result, subtract(20, 5))
+
+console.log(result);
