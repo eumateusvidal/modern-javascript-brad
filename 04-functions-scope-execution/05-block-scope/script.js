3f946c872261abe024ff3572af904d43d437a5db
Mateus Vidal <devmateusvidal@gmail.com>
2025-03-28 15:30:21 -0300

    March 28

--- 04-functions-scope-execution/05-block-scope/script.js
+++ 04-functions-scope-execution/05-block-scope/script.js
@@ -1,27 +1,20 @@
-// window.alert('Hello')
-// console.log(window.innerWidth);
-
 const x = 100;
 
-console.log(x, 'in global');
-
-function run () {
-  console.log(window.innerHeight);
-  console.log(x, 'in function')
-}
-
-run();
-
 if (true) {
-  console.log(x, 'in block');
-}
-
-function add() {
-  const x = 1;
-  const y = 50;
+  const y = 200;
   console.log(x + y);
 }
 
-// console.log(y);
+// console.log(x + y);
 
-add();
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c)
