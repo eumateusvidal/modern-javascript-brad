68d67b988a876be157288de69dccb6982a7db48a
Mateus Vidal <devmateusvidal@gmail.com>
2025-04-07 22:14:53 -0300

    April 7

--- 05-logic-and-control-flow/07-logic-operators/script.js
+++ 05-logic-and-control-flow/07-logic-operators/script.js
@@ -3,10 +3,14 @@ console.log(10 > 20 || 30 < 15);
 
 // && - Will return first falsy value or the last value
 
-let a;
+// let a;
 
-a = 10 && 20;
+// a = 10 && 20;
+// a = 10 && 20 && 30;
+// a = 10 && 0 && 30;
+// a = 10 && '' 0 && 30;
 
-a = 10 && 0 && 30;
+// console.log(a);
 
-console.log(a);
+const posts = ['Post one', 'Post Two']; 
+posts.lenth > 0 && console.log(posts[0]);
