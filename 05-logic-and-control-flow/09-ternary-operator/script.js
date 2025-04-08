c8baff84a00e20c536487c3af3595f6a39c6aa53
Mateus Vidal <devmateusvidal@gmail.com>
2025-04-08 10:03:44 -0300

    April 8

--- 05-logic-and-control-flow/09-ternary-operator/script.js
+++ 05-logic-and-control-flow/09-ternary-operator/script.js
@@ -1,39 +1,37 @@
-//= assigns the right side value only if the left is a falsy value.
+const age = 18;
 
-let a = 10;
-// if (!a) {
-//   a = 10
+// Using an If statement
+
+if (age >= 18) {
+  console.log('You can vote');
+} else {
+  console.log('You can not vote');
+}
+
+// Using a ternary operator
+
+age >= 18 ? console.log('You can vote') : console.log('You can not vote');
+
+// Asssigng a conditional value to a variable
+
+const canVote =  age >= 18 ? true : false;
+const canVote2 =  age >= 18 ? 'You can vote' : 'You can not vote';
+
+console.log(canVote)
+console.log(canVote2)
+
+// Multiple statements
+
+const auth = true;
+// let redirect;
+
+// if (auth) {
+//   alert('Welcome to the dashboard')
+// } else {
+//   alert('Access Denied')
+//   redirect = '/login'
 // }
 
-// a = a || 10;
+const redirect = auth ? (alert('Welcome to the dashboard'), '/dashboard') : (alert('Access denied'), '/login')
 
-a ||= 10;
-
-console.log(a);
-
-//&&= assigns the right side value only if the left is a truthy value.
-
-let b = 10
-
-if (b) {
-  b = 20;
-}
-
-b = b && 20;
-b &&= 20;
-
-console.log(b)
-
-//??= assigns the right side value only if the left is a null or undefined.
-
-let c = null;
-
-if (c === null || c === undefined) {
-  c = 20;
-}
-
-c = c ?? 20;
-
-c ??= 20;
-
-console.log(c)
+console.log(redirect)
