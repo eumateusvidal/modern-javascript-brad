552a38d1e310983763271f5d54d681657657bbdd
Mateus Vidal <devmateusvidal@gmail.com>
2025-04-07 21:54:19 -0300

    April 7

--- 05-logic-and-control-flow/06-truthy-falsy/script.js
+++ 05-logic-and-control-flow/06-truthy-falsy/script.js
@@ -1,7 +1,35 @@
-const email = 'test@teste.com'
+// const email = 'test@teste.com'
 
-if (email) {
-  console.log('You passed is an email');
+// if (email) {
+//   console.log('You passed is an email');
+// }
+
+// console.log(Boolean(email))
+
+// Falsy Values:
+// - false
+// - 0
+// - 
+// - "" or '' (Empty string)
+// - null
+// - undefined
+// - NaN
+
+//Truthy Values
+// - true
+// - '0' (0 in a string)
+// - ' ' (space in a string)
+// - 'false' (false is a string)
+// - [] (empty array)
+// - {} (empty object)
+// - function () {} (empty function)
+
+const x = false;
+
+if (x) {
+  console.log('This is truthy');
+} else {
+  console.log('This is falsy')
 }
 
-console.log(Boolean(email))
+console.log(Boolean(x));
---
+++ 05-logic-and-control-flow/07-test-design/script.js
@@ -0,0 +1,22 @@
+const email = 'test@teste.com'
+
+if (email) {
+  console.log('You passed is an email');
+}
+
+console.log(Boolean(email))
+
+// Falsy Values:
+
+// - false
+// - 0
+// - 
+// - """" or """" Empy
+// - 
+// - 
+// - 
+// - 
+// - 
+// - 
+// - 
+// - 
