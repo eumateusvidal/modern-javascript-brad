f7c922190d094c9230b8b3e5403096299a18b466
Mateus Vidal <devmateusvidal@gmail.com>
2025-04-14 07:02:27 -0300

    April 24

--- 06-loops-iteration-arrays-methods/08-array-for-each/script.js
+++ 06-loops-iteration-arrays-methods/08-array-for-each/script.js
@@ -1,24 +1,5 @@
 const socials = ['Twitter', 'Linkedin', 'Facebook', 'Instagram'];
 
-// console.log(socials.__proto__);
-
-// socials.forEach(function (item){
-//   console.log(item)
-// });
-
-// socials.forEach((item, index, arr) => console.log(`${item} - ${item}`));
-
-function logSocials(social) {
-  console.log(social);
-}
-
-// socials.forEach(logSocials);
-
-const socialObjs = [
-  {name: 'Twitter', url: 'https://twitter.com'},
-  {name: 'Facebook', url: 'https://facebook.com'},
-  {name: 'Linkedin', url: 'https://linkedin.com'},
-  {name: 'Instagram', url: 'https://instagram.com'},
-]
-
-socialObjs.forEach((item) => console.log(item));
+socials.forEach(function (item) {
+  console.log(item);
+});
