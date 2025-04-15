c0d44045bf139dfeeeae5233cb0eaae39ec0648a
Mateus Vidal <devmateusvidal@gmail.com>
2025-04-15 07:43:18 -0300

    April 15

--- 06-loops-iteration-arrays-methods/08-array-for-each/script.js
+++ 06-loops-iteration-arrays-methods/08-array-for-each/script.js
@@ -1,5 +1,20 @@
 const socials = ['Twitter', 'Linkedin', 'Facebook', 'Instagram'];
 
-socials.forEach(function (item) {
-  console.log(item);
-});
+// socials.forEach((item) => console.log(item));
+
+// socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+
+function logSocials(social) {
+  console.log(social);
+};
+
+// socials.forEach(logSocials);
+
+const socialObj = [
+  {name: 'Twitter,', url: 'https://twitter.com' },
+  {name: 'Linkedin,', url: 'https://linkedin.com' },
+  {name: 'Facebook,', url: 'https://facebook.com' },
+  {name: 'Instagram,', url: 'https://instagram.com' },
+];
+
+socialObj.forEach((item) => console.log(item.url));
---
+++ 06-loops-iteration-arrays-methods/09-array-filter/script.js
@@ -0,0 +1,36 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const evenNumbers = numbers.filter(function(number) {
+//   return number % 2 === 0;
+// });
+
+//Short version
+
+// const evenNumbers = numbers.filter((number) => number % 2 === 0)
+
+//Same with forEach
+
+
+// const evenNumbers = [];
+
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     evenNumbers.push(number);
+//   }
+// });
+
+// console.log(evenNumbers);
+
+// Get only retail companies
+
+const companies = [
+  {name: 'Company One', category: 'Finance', start: 1981, end: 2004},
+  {name: 'Company two', category: 'Retail', start: 1992, end: 2008},
+  {name: 'Company Three', category: 'Auto', start: 1999, end: 2007},
+  {name: 'Company Four', category: 'Retail', start: 1989, end: 2010},
+  {name: 'Company Five', category: 'Technology', start: 2009, end: 2014},
+  {name: 'Company Six', category: 'Finance', start: 1987, end: 2010},
+  {name: 'Company Seven', category: 'Auto', start: 1986, end: 2096},
+  {name: 'Company Eight', category: 'Technology', start: 2011, end: 2016},
+  {name: 'Company Nine', category: 'Retail', start: 1981, end: 1989},
+];
