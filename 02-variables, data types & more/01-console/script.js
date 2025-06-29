64f7cccd07d29be4346fd5c47d63a5077137373c
Mateus Vidal <devmateusvidal@gmail.com>
2025-06-29 04:05:58 -0300

    Console Log

--- 02-variables, data types & more/01-console/script.js
+++ 02-variables, data types & more/01-console/script.js
@@ -8,19 +8,13 @@ const x = 100;
 
 console.log(x);
 
-
 console.error('Alert');
 
 console.warn('Warning');
 
-console.table({name: 'Brad', email: 'brad@email.com'});
-
-console.group('simple');
-console.log(x);
-console.error('Alert');
-console.warn('Warning');
+console.table({name: 'Brad', email: 'brad@gmail.com'});
 console.groupEnd();
 
-const styles = 'padding: 10px; background-color: white; color: green;'
+const styles = 'padding: 10px; background-color: white; color: blue';
 
 console.log('%cHello World', styles);
