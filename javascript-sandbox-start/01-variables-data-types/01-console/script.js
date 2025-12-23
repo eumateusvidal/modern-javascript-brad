5ff59609dba44c95d659f22f56b24d82f683b3a4
eumateusvidal <devmateusvidal@gmail.com>
2025-12-22 22:42:15 -0300

    update

---
+++ javascript-sandbox-start/01-variables-data-types/01-console/script.js
@@ -0,0 +1,14 @@
+console.log(100);
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.table({ name: 'Brad', email: 'brad@example.com' });
+
+console.groupEnd();
