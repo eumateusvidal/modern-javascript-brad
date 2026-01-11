78c30a8ae98b83a822f9db91cbc08e4401147eaf
eumateusvidal <devmateusvidal@gmail.com>
2026-01-11 07:49:21 -0300

    strings

---
+++ javascript-sandbox-start/01-variables-data-types/08-type-coercion/script.js
@@ -0,0 +1,21 @@
+let x;
+
+x = 10 + '5';
+
+x = 10 + Number('5');
+
+x = 10 + '5';
+
+x = Number();
+
+x = Number(null);
+
+x = Number(true);
+x = Number(false);
+
+x = 10 + true;
+x = 10 + false;
+
+x = 10 + undefined;
+
+console.log(x, typeof x);
---
+++ javascript-sandbox-start/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,35 @@
+let x;
+
+const name = 'Matt';
+const age = '30';
+
+x = 'Hello, my name is ' + name + ' and I am ' + age + ' years old.';
+
+// Template Literals
+x = `Hello my name is ${name} and I am ${age} years old`;
+
+// String Properties and Methods
+
+const s = new String('Hello World');
+
+x = typeof s;
+
+x = s.length;
+
+//Access value by key
+
+x = s[1];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf('l');
+
+x = s.substring(1, 3);
+x = s.slice(-1);
+
+console.log(x);
