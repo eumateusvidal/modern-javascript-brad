58b7613e68764ceb4052d9c0184c1184627766f5
eumateusvidal <devmateusvidal@gmail.com>
2026-02-06 23:25:30 -0300

    arrays

---
+++ javascript-sandbox-start/02-arrays-and-objects/07-destructuring-naming/script.js
@@ -0,0 +1,35 @@
+const firstName = 'Matt';
+const lastName = 'Vidal';
+const age = 30;
+
+const person = {
+  firstName,
+  lastName,
+  age,
+};
+
+console.log(person.age);
+
+// Destructuring
+
+const todo = {
+  id: 1,
+  title: 'Take out trash',
+  user: {
+    name: 'Matt',
+  },
+};
+
+const {
+  id,
+  title,
+  user: { name },
+} = todo;
+
+// Destructure Arrays
+
+const numbers = [23, 67, 33, 49];
+
+const [first, second] = numbers;
+
+console.log(numbers);
