a07021e2a567274cf660f95d82007cdb89da6614
eumateusvidal <devmateusvidal@gmail.com>
2026-02-14 11:04:22 -0300

    execution-context

--- javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
+++ javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
@@ -6,3 +6,17 @@
 const getCelsius = (f) => ((f - 32) * 5) / 9;
 
 console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+
+// Challenge 2
+
+function minMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return {
+    min,
+    max,
+  };
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
