ad31f81bbbfcc9ab6162a28d8812fbdab313a406
eumateusvidal <devmateusvidal@gmail.com>
2026-02-19 12:49:05 -0300

    update

---
+++ javascript-sandbox-start/05-iteration-array-methods/05-for-of-loop/script.js
@@ -0,0 +1,38 @@
+// Loop through arrays
+
+const items = ['book', 'table', 'chair', 'kite'];
+const users = [
+  { name: 'Matt' },
+  { name: 'Brett' },
+  { name: 'Phillip' },
+  { name: 'Noah' },
+];
+
+// for (let i = 0; i < items.length; i++) {
+//   console.log(items[i]);
+// }
+
+// for (const item of items) {
+//   console.log(item);
+// }
+
+for (const user of users) {
+  console.log(user.name);
+}
+
+// Loop over strings
+const str = 'Hello word';
+
+for (const letter of str) {
+  console.log(letter);
+}
+
+// Loop over Maps
+
+const map = new Map();
+map.set('name', 'John');
+map.set('age', '30');
+
+for (const [key, value] of map) {
+  console.log(key, value);
+}
