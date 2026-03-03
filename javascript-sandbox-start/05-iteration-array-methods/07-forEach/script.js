d1b4e1111abf5dea52491fa77629fb5cc65ddb37
eumateusvidal <devmateusvidal@gmail.com>
2026-03-03 10:59:14 -0300

    arrays-methods

---
+++ javascript-sandbox-start/05-iteration-array-methods/07-forEach/script.js
@@ -0,0 +1,24 @@
+const socials = ['Linkedin', 'X', 'Facebook', 'Instagram'];
+
+// console.log(socials.__proto__);
+
+// socials.forEach(function (item) {
+//   console.log(item);
+// });
+
+// socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+
+// function logSocials(social) {
+//   console.log(social);
+// }
+
+// console.log(logSocials);
+
+const socialObj = [
+  { name: 'Twitter', url: 'https://twitter.com' },
+  { name: 'Facebook', url: 'https://facebook.com' },
+  { name: 'Instagram', url: 'https://instagram.com' },
+  { name: 'Linkedin', url: 'https://linkedin.com' },
+];
+
+socialObj.forEach((item) => console.log(socialObj));
