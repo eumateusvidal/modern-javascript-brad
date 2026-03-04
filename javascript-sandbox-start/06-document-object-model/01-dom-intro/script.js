27c23b33a304fde269b79eb229661146568929c7
eumateusvidal <devmateusvidal@gmail.com>
2026-03-03 22:57:51 -0300

    DOM

---
+++ javascript-sandbox-start/06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,14 @@
+// console.log(window);
+// console.dir(window.document);
+
+// console.log(document.body.innerText);
+
+// console.log(document.links[0]);
+
+// ((document.body.innerHTML = '<h1> Hello World </h1>'), '<a> Hello World <a>');
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+
+main.innerHTML = '<h1>Hello from main</h1>';
